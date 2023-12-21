21a549364c556bc68a3c55ab05f3f23e8dd02eef
seaflower <mikeqoo123@gmail.com>
2023-12-21 14:16:05 +0800

    123456

--- 專案管理/host.cpp
+++ 專案管理/host.cpp
@@ -1,36 +1,48 @@
-#include <algorithm>
+#include "xcl2.hpp"
+#include <vector>
 #include <iostream>
-#include "ap_fixed.h"
+#include <xrt/xrt_device.h>
+#include <experimental/xrt_xclbin.h>
+#include <xrt/xrt_bo.h>
+#include <xrt/xrt_kernel.h>
+#include <experimental/xrt_ip.h>
+#include <experimental/aie.h>
+#include <experimental/graph.h>
 #define DATA_SIZE 4096
+#define INCR_VALUE 10
 
 int main(int argc, char** argv) {
 	if (argc != 2) {
-		std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
-		return -1;
+		std::cout <<"FPGA 執行檔案:" << argv [0] << "<XCLBIN File>" << std::endl;
+		return EXIT_FAILURE;
 	}
 
-	std::string binaryFile = argv[1]; //binary_container_1.xclbin
+	auto binaryFile = argv[1];
+
+	// 分配記憶體
 	cl_int err;
 	cl::Context context;
-	cl::Kernel krnl_matrix_mul;
 	cl::CommandQueue q;
+	cl::Kernel krnl_adder;
+	size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
 
-	ap_int<8>  A[4][4];
-	ap_int<8>  B[4][4];
-	ap_int<16> C[4][4];
+	//aligned_allocator 這是 C++ 的 STL
+	std::vector<int, aligned_allocator<int> > source_input(DATA_SIZE);
+	std::vector<int, aligned_allocator<int> > source_hw_results(DATA_SIZE);
+	std::vector<int, aligned_allocator<int> > source_sw_results(DATA_SIZE);
 
-	// 初始化值
-	for(int i=0;i<4;i++)
-		for(int j=0;j<4;j++) {
-		A[i][j]=i*4+j;
-		B[i][j]=A[i][j];
-		std::cout << "A[" << i << "," << j << "]=" << A[i][j];
-		std::cout << ", B[" << i << "," << j << "]=" << B[i][j] << std::endl;
+	// 建立測試數據和軟體結果
+	for (int i = 0; i < DATA_SIZE; i++) {
+		source_input[i] = i;
+		source_sw_results[i] = i + INCR_VALUE;
+		source_hw_results[i] = 0;
+		std::cout << "source_input=" << source_input[i] << "|source_sw_results=" << source_sw_results[i] << "|source_hw_results=" << source_hw_results[i] << std::endl;
 	}
 
-	// 拿 U50
+	// 去拿板子資訊
 	auto devices = xcl::get_xil_devices();
-	// 讀執行檔
+
+	// 板子內核的讀取
 	auto fileBuf = xcl::read_binary_file(binaryFile);
 	cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
 	bool valid_device = false;
@@ -39,19 +51,62 @@ int main(int argc, char** argv) {
 		// Creating Context and Command Queue for selected Device
 		OCL_CHECK(err, context = cl::Context(device, nullptr, nullptr, nullptr, &err));
 		OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+
 		std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
 		cl::Program program(context, {device}, bins, nullptr, &err);
 		if (err != CL_SUCCESS) {
 			std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
 		} else {
 			std::cout << "Device[" << i << "]: program successful!\n";
-			OCL_CHECK(err, krnl_vector_add = cl::Kernel(program, "matrix_mul", &err));
+			OCL_CHECK(err, krnl_adder = cl::Kernel(program, "adder", &err));
 			valid_device = true;
 			break; // we break because we found a valid device
 		}
 	}
 	if (!valid_device) {
 		std::cout << "Failed to program any device found, exit!\n";
-		return -1;
+		exit(EXIT_FAILURE);
 	}
+
+	// Allocate Buffer in Global Memory
+	OCL_CHECK(err, cl::Buffer buffer_input(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, vector_size_bytes,
+										   source_input.data(), &err));
+	OCL_CHECK(err, cl::Buffer buffer_output(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, vector_size_bytes,
+											source_hw_results.data(), &err));
+
+	int inc = INCR_VALUE;
+	int size = DATA_SIZE;
+	// 設定核心的參數
+	int narg = 0;
+	OCL_CHECK(err, err = krnl_adder.setArg(narg++, buffer_input));
+	OCL_CHECK(err, err = krnl_adder.setArg(narg++, buffer_output));
+	OCL_CHECK(err, err = krnl_adder.setArg(narg++, inc));
+	OCL_CHECK(err, err = krnl_adder.setArg(narg++, size));
+
+	// 把 input 資料丟到板子的記憶體
+	OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_input}, 0 /* 0 means from host*/));
+
+	// 啟動核心
+	OCL_CHECK(err, err = q.enqueueTask(krnl_adder));
+
+	// 把板子上的結果複製到本地端
+	OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output}, CL_MIGRATE_MEM_OBJECT_HOST));
+	q.finish();
+
+	// OPENCL HOST CODE AREA END
+
+	// 比對結果
+	int match = 0;
+	for (int i = 0; i < DATA_SIZE; i++) {
+		if (source_hw_results[i] != source_sw_results[i]) {
+			std::cout << "Error: Result mismatch" << std::endl;
+			std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
+					  << " Device result = " << source_hw_results[i] << std::endl;
+			match = 1;
+			break;
+		}
+	}
+
+	std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
+	return (match ? EXIT_FAILURE : EXIT_SUCCESS);
 }
--- 專案管理/kernel.cpp
+++ 專案管理/kernel.cpp
@@ -1,25 +1,87 @@
-extern "C" {
-void matrix_mul(ap_int<8> A[4][4], ap_int<8> B[4][4], ap_int<16> C[4][4]) {
-    #pragma HLS ARRAY_RESHAPE dim=2 type=complete variable=A
-    #pragma HLS ARRAY_RESHAPE dim=1 type=complete variable=B
-    #pragma HLS INTERFACE m_axi port = A bundle = gmem0
-    #pragma HLS INTERFACE m_axi port = B bundle = gmem1
-    #pragma HLS INTERFACE m_axi port = C bundle = gmem0
+#include <ap_int.h>
+#include <hls_stream.h>
 
-	row: for(int i=0;i<4;i++) {
-        #pragma HLS PIPELINE off
-        #pragma HLS INLINE off
-		col: for(int j=0;j<4;j++) {
-            #pragma HLS PIPELINE II=1
-            #pragma HLS INLINE off
-			C[i][j]=0;
-			// 循環乘四次，在相加
-			product: for(int k=0;k<4;k++) {
-                #pragma HLS PIPELINE off
-                #pragma HLS INLINE off
-				C[i][j]=C[i][j]+A[i][k]*B[k][j];
-			}
-		}
-	}
+#define DATA_SIZE 4096
+
+// TRIPCOUNT identifier
+const int c_size = DATA_SIZE;
+
+// 從記憶體讀取資料並寫入 inStream
+static void read_input(unsigned int *in, hls::stream<unsigned int> &inStream, int size)
+{
+// Auto-pipeline is going to apply pipeline to this loop
+mem_rd:
+    for (int i = 0; i < size; i++)
+    {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking write command to inStream
+        inStream << in[i];
+    }
 }
+
+// 從 inStream 讀取輸入資料並將結果寫入 outStream
+static void compute_add(hls::stream<unsigned int> &inStream, hls::stream<unsigned int> &outStream, int inc, int size)
+{
+// Auto-pipeline is going to apply pipeline to this loop
+execute:
+    for (int i = 0; i < size; i++)
+    {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking read command from inStream and Blocking write command to outStream
+        outStream << (inStream.read() + inc);
+    }
+}
+
+// 把結果寫入 outStream 傳回去
+static void write_result(unsigned int *out, hls::stream<unsigned int> &outStream, int size)
+{
+// Auto-pipeline is going to apply pipeline to this loop
+mem_wr:
+    for (int i = 0; i < size; i++)
+    {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking read command to inStream
+        out[i] = outStream.read();
+    }
+}
+
+extern "C"
+{
+    /*
+        Vector Addition Kernel Implementation using dataflow
+        Arguments:
+            in   (input)  --> Input Vector
+            out  (output) --> Output Vector
+            inc  (input)  --> Increment
+            size (input)  --> Size of Vector in Integer
+       */
+    void adder(unsigned int *in, unsigned int *out, int inc, int size)
+    {
+        // Adding names for the streams. It allows the name to be used in reporting.
+        // Vivado HLS
+        // automatically checks to ensure all elements from an input stream are read
+        // during sw emulation.
+        static hls::stream<unsigned int> inStream("input_stream");
+        static hls::stream<unsigned int> outStream("output_stream");
+#pragma HLS STREAM variable = inStream depth = 32
+#pragma HLS STREAM variable = outStream depth = 32
+        //  HLS STREAM variable=<name> depth=<size> pragma is used to define the Stream
+        //  depth. For this example, Depth 32 is defined. Which means that Stream can
+        //  hold
+        //  maximum 32 outstanding elements at a given time. If Stream is full, any
+        //  further
+        //  blocking write command from producer will go into wait state until consumer
+        //  reads some elements from stream. Similarly if Stream is empty (no element in
+        //  Stream)
+        //  any blocking read command from consumer will go into wait state until
+        //  producer
+        //  writes elements to Stream. This blocking read and write allow consumer and
+        //  producer to synchronize each other.
+
+#pragma HLS dataflow
+        // dataflow pragma instruct compiler to run following three APIs in parallel
+        read_input(in, inStream, size);
+        compute_add(inStream, outStream, inc, size);
+        write_result(out, outStream, size);
+    }
 }
